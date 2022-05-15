231c8e38909f712a25956cb7821c535b078c6fcb
fadyfadd <fady_faddoul@hotmail.com>
2022-05-15 10:03:26 -0400

    adding certificates

--- API/Program.cs
+++ API/Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
   
 
 app.UseHttpsRedirection();
- app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
