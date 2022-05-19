46c27428992dbc079be7545dbe594f923e9b709b
fadyfadd <fady_faddoul@hotmail.com>
2022-05-18 20:14:55 -0400

    authenticating user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,29 +12,58 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        
+
         public AccountController(DataContext context)
         {
-             
+
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(String userName , String password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-               using (var hmac = new HMACSHA512()) {
-               var user = new AppUser() {
-                  UserName = userName,
-                  PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-                  PasswordSalt = hmac.Key
-               };
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-               this._context.Users.Add(user);
-               await this._context.SaveChangesAsync();
-               return user;
-           }
-           
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+
         }
-         
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+               .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user; 
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
     }
 }
