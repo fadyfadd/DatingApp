ac1e4793280de0e3349cd2c3cb873d140f6074da
fadyfadd <fady_faddoul@hotmail.com>
2022-05-25 19:51:50 -0400

    angular routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,20 +3,28 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
+import {  MemberListComponent} from "./members/member-list/member-list.component"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailsComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +32,7 @@ import { RegisterComponent } from './register/register.component';
     AppRoutingModule,
     HttpClientModule,
     BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({positionClass:"toast-bottom-right"} ),
     BrowserAnimationsModule
   ],
   providers: [],
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-nav',
@@ -11,20 +14,24 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService , private router: Router ,  private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    this.accountService.login(this.model).subscribe(response => {
+    this.accountService.login(this.model).subscribe(response => {     
+      
+      this.router.navigateByUrl('/members')
       console.log(response);
-    }, error => {
-      console.log(error);
+    }, error => {   
+      this.toastr.error(error.message);
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/')
+    
   }
 }
