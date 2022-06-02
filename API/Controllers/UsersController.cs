e5530d0296d7ef57af92ee9a9ce5c722bf5bb129
fadyfadd <fady_faddoul@hotmail.com>
2022-06-01 20:09:01 -0400

    more about mapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -12,34 +12,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  
     [Authorize]
     public class UsersController : BaseApiController
     {
-         private readonly IUserRepository _userRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        
         public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-             _userRepository = userRepository;
             _mapper = mapper;
+            _userRepository = userRepository;
         }
 
-         
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users =  await _userRepository.GetUsersAsync();
-            var mappedUsers = _mapper.Map<IEnumerable<MemberDto>>(users) ;
-            return mappedUsers.ToList();
+            var users = await _userRepository.GetMembersAsync();
+
+            return Ok(users);
         }
 
-      
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await _userRepository.GetUserByUsernameAsync(username);
-            return _mapper.Map<MemberDto>(user);
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -4,29 +4,35 @@ using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
-
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Data
 {
     public class UserRepository : IUserRepository
     {
         private readonly DataContext _context;
-     
-        public UserRepository(DataContext context)
+        private readonly IMapper _mapper;
+        public UserRepository(DataContext context, IMapper mapper)
         {
-           
+            _mapper = mapper;
             _context = context;
         }
 
         public async Task<MemberDto> GetMemberAsync(string username)
-        {
-           throw new NotImplementedException("Not Implemented");
+        { 
+            return await _context.Users
+                .Where(x => x.UserName == username)
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
         }
 
         public async Task<IEnumerable<MemberDto>> GetMembersAsync()
         {
-           throw new NotImplementedException("Not Implemented");
+            return await _context.Users 
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
@@ -36,15 +42,15 @@ namespace API.Data
 
         public async Task<AppUser> GetUserByUsernameAsync(string username)
         {
-            return await _context.Users.Include(p=>p.Photos)
-                
+            return await _context.Users
+                .Include(p => p.Photos)
                 .SingleOrDefaultAsync(x => x.UserName == username);
         }
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
-            return await _context.Users.Include(p=>p.Photos)
-                
+            return await _context.Users
+                .Include(p => p.Photos)
                 .ToListAsync();
         }
 
