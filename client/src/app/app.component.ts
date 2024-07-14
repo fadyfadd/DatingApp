195861216446b255fab831ae5664a98d5548c714
fadyfadd <fady_faddoul@hotmail.com>
2024-07-14 08:08:12 +0300

    202407140808

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,29 @@
-import { CommonModule, NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+//import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet , CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavComponent]
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
+  private accountService = inject(AccountService);
+
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    this.setCurrentUser();
   }
 
-  http = inject(HttpClient);
-  title = 'DatingApp';
-  users: any;
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+
 }
